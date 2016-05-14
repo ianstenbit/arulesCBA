961eb2afda49dc4412205c8e6734a63a04943039
Ian Johnson <ianjjohnson@icloud.com>
2016-05-13 21:10:25 -0600

    Add basic commenting for CBA function

--- CBA_Function.R
+++ CBA_Function.R
@@ -5,6 +5,7 @@ setwd("~/Dropbox/Summer 2016/CSE 5390/CBA_Algorithm/")
 
 
 load("titanic.raw.rdata")
+head(titanic.raw)
 classifier <- CBA(titanic.raw, "Survived", apriori_parameter = list(minlen=2, supp = 0.05, conf=0.9))
 results <- classify(titanic.raw, classifier)
 CrossTable(x = titanic.raw$Survived, y = results, prop.chisq = FALSE)
@@ -16,7 +17,7 @@ dataset$id <- NULL
 dsDisc <- as.data.frame(lapply(dataset[2:31], function(x) discretize(x, categories = 5)))
 dsDisc$diagnosis <- factor(dataset$diagnosis)
 dsDisc <- dsDisc[c(1:10, 31)]
-classifier <- CBA(dsDisc, "diagnosis", apriori_parameter = list(minlen=2, supp = 0.05, conf=0.75))
+classifier <- CBA(dsDisc, "diagnosis", apriori_parameter = list(minlen=2, supp = 0.05, conf=0.9))
 results <- classify(dsDisc, classifier)
 CrossTable(x = dsDisc$diagnosis, y = results, prop.chisq = FALSE)
 
@@ -33,38 +34,53 @@ CrossTable(x=irisDisc$Species, y=results, prop.chisq = FALSE)
 
 CBA <- function(dataset, column, apriori_parameter){
   
+  #Generate and sort association rules
   rules <- apriori(dataset, parameter = apriori_parameter, appearance = list(rhs=paste(column, levels(dataset[,column]), sep="="), default = "lhs"), control=list(verbose=FALSE))
   rules.sorted <- sort(rules, by=c("confidence", "support", "lift"))
 
+  #Prune association rule set to remove redundant rules
   subset.matrix <- is.subset(rules.sorted, rules.sorted)
   subset.matrix[lower.tri(subset.matrix, diag = TRUE)] <- NA
   redundant <- colSums(subset.matrix, na.rm = TRUE) >= 1
   rules.sorted <- rules.sorted[!redundant]
 
+  #Build transaction matrix from dataset
   ds.mat <- as(dataset, 'transactions')
 
+  #Vector used to identify rules as being 'strong' rules for the final classifier 
   strongRules <- vector('logical', length=length(rules.sorted))
 
+  #Remaining rhs of transaction records, used to calculate default class
   rightHand <- dataset[,column]
 
+  #For all of the pruned, sorted rules
   for(i in 1:length(rules.sorted)){
   
-    outcome <-as(rules.sorted[i]@rhs, "ngCMatrix")[10,]
+    #List of transactions which match this rule
     matches <- is.subset(rules.sorted[i]@lhs, ds.mat)
   
+    #Remove matching transactions from remaining dataset
     ds.mat <- ds.mat[!matches,]
     rightHand <- rightHand[!matches]
   
+    #If no transactions match this rule, skip it
     if(length(matches) == 0){next}
   
+    #Save this rule as being a strong rule if it matched any transaction
     strongRules[i] <- Reduce('|', matches)
+    
+    #Save the default class after this rule
     defaultClass <- sort(table(rightHand), decreasing=TRUE)[1]
   
   }
 
+  #Take the subset of pruned, sorted rules which are considered strong. These are our classifier
   classifier <- rules.sorted[strongRules]
+  
+  #Add a default class to the classifier
   df <- paste(column, names(defaultClass), sep="=")
   classifier <- list(classifier, df)
+  
   return(classifier)
   
 }
