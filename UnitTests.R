939a708cfcdd3144725bda24d89a93d2597485ae
Ian Johnson <ianjjohnson@icloud.com>
2016-05-14 13:54:40 -0600

    Add UnitTests.R for unit tests, removed from main function file

---
+++ UnitTests.R
@@ -0,0 +1,29 @@
+require(gmodels)
+#setwd("PUT WORKING DIRECTORY HERE")
+setwd("~/Dropbox/Summer 2016/CSE 5390/CBA_Algorithm/")
+
+#Test data set 1: Titanic survival
+load("titanic.raw.rdata")
+classifier <- CBA.2(titanic.raw, "Survived", apriori_parameter = list(minlen=2, supp = 0.05, conf=0.9))
+results <- classify(titanic.raw, classifier)
+CrossTable(x = titanic.raw$Survived, y = results, prop.chisq = FALSE, prop.r = FALSE, prop.c = FALSE, prop.t = FALSE)
+
+
+#Test data set 2: Benign and Malignant tumors
+dataset <- read.csv("wisc_bc_data.csv", stringsAsFactors = TRUE)
+dataset$id <- NULL
+dsDisc <- as.data.frame(lapply(dataset[2:31], function(x) discretize(x, categories = 5)))
+dsDisc$diagnosis <- factor(dataset$diagnosis)
+dsDisc <- dsDisc[c(1:10, 31)]
+classifier <- CBA.2(dsDisc, "diagnosis", apriori_parameter = list(minlen=2, supp = 0.05, conf=0.9))
+results <- classify(dsDisc, classifier)
+CrossTable(x = dsDisc$diagnosis, y = results, prop.chisq = FALSE, prop.r = FALSE, prop.c = FALSE, prop.t = FALSE)
+
+
+#Test data set 3: Flower species classification
+data(iris)
+irisDisc <- as.data.frame(lapply(iris[1:4], function(x) discretize(x, categories=5)))
+irisDisc$Species <- iris$Species
+classifier <- CBA.2(irisDisc, "Species", apriori_parameter = list(minlen=2, supp = 0.05, conf=0.9))
+results <- classify(irisDisc, classifier)
+CrossTable(x=irisDisc$Species, y=results, prop.chisq = FALSE, prop.r = FALSE, prop.c = FALSE, prop.t = FALSE)
