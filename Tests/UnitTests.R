386ad5fc7544ea7b249f52472ea875b4902717b6
Ian Johnson <ianjjohnson@mac.com>
2016-05-17 10:49:22 -0600

    Refactoring

---
+++ Tests/UnitTests.R
@@ -0,0 +1,36 @@
+require(gmodels)
+setwd("PUT WORKING DIRECTORY HERE")
+
+#Test data set 1: Titanic survival
+load("titanic.raw.rdata")
+classifier <- CBA.2(titanic.raw, "Survived", apriori_parameter = list(minlen=2, supp = 0.05, conf=0.6))
+results <- classify(titanic.raw, classifier)
+CrossTable(x = titanic.raw$Survived, y = results, prop.chisq = FALSE, prop.r = FALSE, prop.c = FALSE, prop.t = FALSE)
+
+
+#Test data set 2: Benign and Malignant tumors
+dataset <- read.csv("wisc_bc_data.csv", stringsAsFactors = TRUE)
+dataset$id <- NULL
+dsDisc <- as.data.frame(lapply(dataset[2:31], function(x) discretize(x, categories = 6)))
+dsDisc$diagnosis <- factor(dataset$diagnosis)
+dsDisc <- dsDisc[c(1:10, 31)]
+classifier <- CBA.2(dsDisc, "diagnosis", apriori_parameter = list(minlen=2, supp = 0.05, conf=0.9))
+results <- classify(dsDisc, classifier)
+CrossTable(x = dsDisc$diagnosis, y = results, prop.chisq = FALSE, prop.r = FALSE, prop.c = FALSE, prop.t = FALSE)
+
+
+#Test data set 3: Flower species classification
+data(iris)
+irisDisc <- as.data.frame(lapply(iris[1:4], function(x) discretize(x, categories=9)))
+irisDisc$Species <- iris$Species
+classifier <- CBA.2(irisDisc, "Species", apriori_parameter = list(minlen=2, supp = 0.05, conf=0.9))
+results <- classify(irisDisc, classifier)
+CrossTable(x=irisDisc$Species, y=results, prop.chisq = FALSE, prop.r = FALSE, prop.c = FALSE, prop.t = FALSE)
+
+#Test data set 4: Congressional voting records
+congressData <- read.csv("house-votes-84.data", stringsAsFactors=TRUE, header=FALSE)
+colnames(congressData)[1] <- "Party"
+head(congressData)
+classifier <- CBA.2(congressData, "Party", list(minlen=2, supp = 0.10, conf=0.99))
+results <- classify(congressData, classifier)
+CrossTable(x=congressData$Party, y=results, prop.chisq = FALSE, prop.r = FALSE, prop.c = FALSE, prop.t = FALSE)
