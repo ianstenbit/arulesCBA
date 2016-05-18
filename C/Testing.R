4bde2c76f5b4abbaa8f8421fb7210fa7016e01e3
Ian Johnson <ianjjohnson@icloud.com>
2016-05-17 21:45:22 -0600

    Add Parts I and II of CBA algorithm in C

--- C/Testing.R
+++ C/Testing.R
@@ -15,17 +15,113 @@ CBA.2 <- function(dataset, column, apriori_parameter, verbose=FALSE){
   rules.sorted <- rules.sorted[!redundant]
   
   #Build transaction matrix from dataset
-  #ds.mat <- as(dataset, 'transactions')
-  ds.mat <- dataset
+  ds.mat <- as(dataset, 'transactions')
+  
+  #Vector used to identify rules as being 'strong' rules for the final classifier 
+  strongRules <- vector('logical', length=length(rules.sorted))
   
   #right-hand side of data records
   rightHand <- dataset[,column]
   
+  rulesMatchLHS <- is.subset(rules.sorted@lhs, ds.mat)
+  rulesMatchRHS <- is.subset(rules.sorted@rhs, ds.mat)
+  
+  #matrix of rules and records which constitute correct and false matches
+  matches <- rulesMatchLHS & rulesMatchRHS
+  falseMatches <- rulesMatchLHS & !rulesMatchRHS
+  
+  #a is a set of wrongly classified records
+  #a <- data.frame(dID=integer(),
+  #                crule=integer(),
+  #                wrule=integer())
+  
+  #matrix of rules and classification factor to identify how many times the rule correctly identifies the class
+  casesCovered <- vector('integer', length=length(rules.sorted))
+  
+  strongRules <- vector('logical', length=length(rules.sorted))
+  
   setwd("~/Dropbox/Summer 2016/CSE 5390/CBA_Algorithm/C")
   dyn.load("test.so")
-  .Call("test", ds.mat, rightHand, rules.sorted, nrow(ds.mat), length(rules))
+  a <- .Call("stage1", dataset, strongRules, casesCovered, matches, falseMatches, length(rules.sorted), grep(column, colnames(dataset)))
   
-  return(1)
+  replace <- .Call("stage2", a, casesCovered, matches, strongRules)
+  
+  #initializing variables for stage 3
+  ruleErrors <- 0
+  classDistr <- dataset[,column]
+  
+  covered <- vector('logical', length=length(ds.mat))
+  covered[1:length(ds.mat)] <- FALSE
+  
+  replace <- as.data.frame(matrix(replace, ncol=3))
+  colnames(replace) <- c("rule", "replaceRule", "dID")
+  replace$rule <- as.numeric(replace$rule)
+  
+  defaultClasses <- vector('character', length=length(rules.sorted))
+  totalErrors <- vector('numeric', length=length(rules.sorted))
+  
+  ####Stage 3####
+  
+  #for every rule in the sorted list of rules
+  for(i in 1:length(rules.sorted)){
+    
+    #if this rule was marked for use in the classifier
+    if(strongRules[i]){ 
+      
+      #if this rule no longer correctly identifies at least one thing, remove it from the list of rules for classifier
+      if(casesCovered[i] == 0){
+        strongRules[i] <- FALSE
+        next
+      }
+      
+      #get list of possible rules to replace this rule
+      rule.replace <- replace[replace$rule == i,]
+      
+      #if there are possible replacements
+      if(nrow(rule.replace) > 0){
+        
+        # for every possible replacement
+        for(j in 1:nrow(rule.replace)){
+          
+          #if the entry for which this rule has been labeled a possible replacement has already been classified by the classifier, decrement its correct classification
+          if(covered[as.integer(rule.replace$dID[j])]){
+            casesCovered[i] <- casesCovered[i] - 1
+          } 
+          #if it hasn't been covered, decrement correct classification counter for replacement rule
+          else {
+            casesCovered[rule.replace$rule[j]] <- casesCovered[rule.replace$rule[j]]  - 1
+          }
+        }
+      }
+      
+      #recognize which entried we've now covered with the current rule
+      covered <- covered | matches[i,]
+      
+      #save the remaining distribution of classes of those entries which haven't been classified
+      classDistr <- dataset[,column][!covered]
+      
+      #add all of the false matches of this rule to the rule error count
+      ruleErrors <- ruleErrors + sum(falseMatches[i,] == TRUE)
+      
+      #save the default class and calculate how many errors use of that defualt class will generate
+      defaultClass <- names(sort(table(classDistr), decreasing=TRUE)[1])
+      defaultErrors <- sum(sort(table(classDistr), decreasing=TRUE)[2:length(table(classDistr))])
+      
+      #save the total number of errors for the current classifier and the default class at this stage
+      totalErrors[i] <- ruleErrors + defaultErrors
+      defaultClasses[i] <- defaultClass
+    }
+  }
+  
+  #save the classifier as only the rules up to the point where we have the lowest total error count
+  classifier <- rules.sorted[strongRules][1:which.min(totalErrors[strongRules])]
+  
+  #add a default class to the classifier (the default class from the last rule included in the classifier)
+  defaultClass <- defaultClasses[strongRules][which.min(totalErrors[strongRules])]
+  df <- paste(column, defaultClass, sep="=")
+  classifier <- list(classifier, df)
+  
+  return(classifier)
 
 }
 
--- C/test.c
+++ C/test.c
@@ -1,5 +1,9 @@
 #include <R.h>
 #include <Rdefines.h>
+#include <Rinternals.h>
+
+#define _ind(x,y,cols) (x) + (y) * (cols)
+
 SEXP test_func(SEXP input) {
   SEXP p1;
   PROTECT(p1 = NEW_INTEGER(1));
@@ -19,3 +23,123 @@ SEXP test(SEXP ds, SEXP rightHand, SEXP rules, SEXP numRows, SEXP numRules){
 
 	return ds;
 }
+
+int firstMatch(int* matrix, int entry_row, int numRules, int numEntries){
+	for(int i = 0; i < numRules; i++)
+		if(matrix[_ind(i, entry_row, numRules)] == 1)
+			return i;
+
+	return -1;
+}
+
+int* getMatches(int* matrix, int entry_row, int numRules){
+
+	int* r = malloc(numRules * sizeof *r);
+
+	for(int i = 0; i < numRules; i++){
+		if(matrix[_ind(i, entry_row, numRules)] == 1)
+			r[i] = 1;
+		else
+			r[i] = 0;
+	}
+
+	return r;
+}
+
+SEXP stage1(SEXP dataset, SEXP strong_rules, SEXP casesCovered, SEXP matches, SEXP falseMatches, SEXP numRules, SEXP classify_column){
+
+	R_len_t i, nrows, ncols, nrules;
+	int crule, wrule, classify;
+	ncols = length(dataset);
+	nrules = INTEGER(numRules)[0];
+	nrows = length(getAttrib(dataset, R_RowNamesSymbol));
+	classify = INTEGER(classify_column)[0];
+
+	int* matchMatrix = INTEGER(matches);
+	int* falseMatchMatrix = INTEGER(falseMatches);
+
+	int* a_vector = malloc(nrows * 3 * sizeof *a_vector);
+	int a_size = 0;
+
+	for(i = 0; i < nrows; i++){
+		crule = firstMatch(matchMatrix, i, nrules, nrows);
+		wrule = firstMatch(falseMatchMatrix, i, nrules, nrows);
+
+		if(crule != -1){
+			INTEGER(casesCovered)[crule]++;
+		}
+
+    	
+    	if(crule > wrule){
+    		LOGICAL(strong_rules)[crule] = TRUE;
+    	} else if (wrule > crule){
+      		a_vector[a_size++] = i;
+      		a_vector[a_size++] = crule;
+      		a_vector[a_size++] = wrule;
+    	}
+		
+	}
+
+	SEXP a = allocVector(INTSXP, a_size);
+	for(i = 0; i < a_size; i++){
+		INTEGER(a)[i] = a_vector[i];
+	}
+
+	return a;
+}
+
+SEXP stage2(SEXP a, SEXP casesCovered, SEXP matches, SEXP strong_rules){
+
+	int a_length = length(a);
+	int entry, crule, wrule;
+
+	int numRules = length(strong_rules);
+
+	int* a_arr = INTEGER(a);
+	int* strong_rules_arr = LOGICAL(strong_rules);
+	int* cases_covered_arr = INTEGER(casesCovered);
+	int* matches_matrix = INTEGER(matches);
+
+	int* replace = malloc(3*a_length*numRules * sizeof *replace);
+	int replaceSize = 0;
+
+	for(int i = 0; i < a_length; i+=3){
+
+		entry = a_arr[i];
+		crule = a_arr[i+1];
+		wrule = a_arr[i+2];
+
+		if(strong_rules_arr[wrule]){
+			if(crule != -1) cases_covered_arr[crule]--;
+			cases_covered_arr[wrule]++;
+		} else {
+
+			int* wSet = getMatches(matches_matrix, entry, numRules);
+
+			for(int j = 0; j < numRules; j++){
+				if(wSet[j] == 0 || j == crule) continue;
+
+				strong_rules_arr[j] = TRUE;
+
+				replace[replaceSize++] = crule;
+      			replace[replaceSize++] = j;
+      			replace[replaceSize++] = entry;
+
+			}
+
+		}
+	}
+
+	SEXP rep = allocVector(INTSXP, replaceSize);
+	for(int i = 0; i < replaceSize; i++){
+		INTEGER(rep)[i] = replace[i];
+	}
+
+	return rep;
+
+}
+
+SEXP stage3(SEXP strong_rules, SEXP casesCovered){
+
+}
+
