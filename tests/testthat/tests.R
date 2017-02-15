d2cc21e94e18f49557b5f96d6d69d954820ed151
Ian Johnson <ij502y@att.com>
2017-02-15 13:01:39 -0600

    Update tests

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -10,7 +10,7 @@ irisDisc$Species <- iris$Species
 classifier <- CBA(Species ~ ., irisDisc, supp = 0.05, conf=0.9)
 results <- predict(classifier, irisDisc)
 
-expect_equal(length(classifier$rules), 3L)
+expect_equal(length(classifier$rules), 11L)
 expect_equal(classifier$default, "Species=versicolor")
 expect_equal(results[1], factor("setosa",
   levels = c("setosa", "versicolor", "virginica")))
