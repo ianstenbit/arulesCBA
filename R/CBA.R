d36c3d5a7f4cac598ff4eb83066ee5dfa5685f44
Ian Johnson <ianjjohnson@icloud.com>
2017-02-23 08:45:06 -0600

    fix interface functions

--- R/CBA.R
+++ R/CBA.R
@@ -1,7 +1,7 @@
-CBA <- function(formula, data, support = 0.2, confidence = 0.8, gamma = 0.05, cost = 10.0,
+CBA <- function(formula, data, support = 0.2, confidence = 0.8,
   verbose=FALSE, parameter = NULL, control = NULL){
 
-    return(CBA.internal(formula, data, method="CBA" support = 0.2, confidence = 0.8, gamma = 0.05, cost = 10.0,
+    return(CBA.internal(formula, data, method="CBA", support = support, confidence = confidence,
       verbose=FALSE, parameter = NULL, control = NULL))
 
 }
