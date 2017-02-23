5f84db15d97840f0201dac1907ad39c3a001b707
Ian Johnson <ianjjohnson@icloud.com>
2017-02-23 08:10:37 -0600

    Refactor CBA/bCBA for different algorithms

---
+++ R/CBAInternal.R
@@ -0,0 +1,122 @@
+CBA.internal <- function(formula, data, method="weighted", support = 0.2, confidence = 0.8, gamma = 0.05, cost = 10.0,
+  verbose=FALSE, parameter = NULL, control = NULL){
+
+  description <- paste0("CBA algorithm by Liu, et al. 1998 with support=", support,
+    " and confidence=", confidence)
+
+  if(is.null(parameter)) parameter <- list()
+  parameter$support <- support
+  parameter$confidence <- confidence
+  parameter$minlen <- 2
+
+  if(is.null(control)) control <- list()
+  control$verbose <- verbose
+
+  # find class
+  formula <- as.formula(formula)
+  class <- as.character(formula[[2]])
+  ### FIXME: we could allow using only a subset of items here!
+  if(as.character(formula[[3]]) != ".")
+    stop("Formula needs to be of the form class ~ .")
+
+
+  ####Preparing data####
+  lvls <- NULL
+  if(is(data, "data.frame")) lvls <- levels(data[[class]])
+
+  ds.mat <- as(data, "transactions")
+  info <- itemInfo(ds.mat)
+  classNames <- info[info$variables == class,'labels']
+  if(is.null(lvls)) lvls <- sapply(strsplit(classNames,"="),'[',2)
+
+  rightHand <- as(ds.mat[, classNames], "list")
+  if(!all(sapply(rightHand, length) == 1L)) stop("Problem with items used for class. Examples with multiple/no class label!")
+  rightHand <- as.factor(unlist(rightHand))
+
+  #Generate and sort association rules
+  rules <- apriori(ds.mat, parameter = parameter,
+    appearance = list(rhs=levels(rightHand), default = "lhs"),
+    control=control)
+
+
+  if(method == "CBA"){
+
+    rules.sorted <- sort(rules, by=c("confidence", "support", "lift"))
+
+    #Vector used to identify rules as being 'strong' rules for the final classifier
+    strongRules <- vector('logical', length=length(rules.sorted))
+
+    rulesMatchLHS <- is.subset(lhs(rules.sorted), ds.mat, sparse = TRUE)
+    rulesMatchRHS <- is.subset(rhs(rules.sorted), ds.mat, sparse = TRUE)
+
+    #matrix of rules and records which constitute correct and false matches
+    matches <- rulesMatchLHS & rulesMatchRHS
+    falseMatches <- rulesMatchLHS & !rulesMatchRHS
+
+
+    #matrix of rules and classification factor to identify how many times the rule correctly identifies the class
+    casesCovered <- vector('integer', length=length(rules.sorted))
+
+    strongRules <- vector('logical', length=length(rules.sorted))
+
+    a <- .Call("stage1", length(ds.mat), strongRules, casesCovered, matches@i, matches@p, length(matches@i), falseMatches@i, falseMatches@p, length(falseMatches@i), length(rules.sorted), PACKAGE = "arulesCBA")
+
+    replace <- .Call("stage2", a, casesCovered, matches@i, matches@p, length(matches@i), strongRules, length(matches@p), PACKAGE = "arulesCBA")
+
+    #initializing variables for stage 3
+    ruleErrors <- 0
+    classDistr <- as.integer(rightHand)
+
+    covered <- vector('logical', length=length(ds.mat))
+    covered[1:length(ds.mat)] <- FALSE
+
+    defaultClasses <- vector('integer', length=length(rules.sorted))
+    totalErrors <- vector('integer', length=length(rules.sorted))
+
+    .Call("stage3", strongRules, casesCovered, covered, defaultClasses, totalErrors, classDistr, replace,matches@i, matches@p, length(matches@i), falseMatches@i, falseMatches@p, length(falseMatches@i), length(levels(rightHand)),  PACKAGE = "arulesCBA")
+
+    #save the classifier as only the rules up to the point where we have the lowest total error count
+    classifier <- rules.sorted[strongRules][1:which.min(totalErrors[strongRules])]
+
+    #add a default class to the classifier (the default class from the last rule included in the classifier)
+    defaultClass <- levels(rightHand)[defaultClasses[strongRules][[which.min(totalErrors[strongRules])]]]
+
+    classifier <- list(
+      rules = classifier,
+      class = class,
+      levels = lvls,
+      default = defaultClass,
+      description = description,
+      method = "first"
+    )
+
+  } else if(method == "weighted") {
+
+    rules.sorted <- sort(rules, by=c("lift", "confidence", "support"))
+
+    rules.sorted <- rules.sorted[1:min(length(rules.sorted), 50000)]
+
+    rule_weights <- rep(0, length(rules.sorted))
+
+    defaultClass <- .Call("weighted", rule_weights, rules.sorted@lhs@data@i, rules.sorted@lhs@data@p, rules.sorted@rhs@data@i, ds.mat@data@i, ds.mat@data@p, ds.mat@data@Dim, gamma, cost, length(levels(rightHand)))
+
+    classifier <- list(
+      rules = rules.sorted[rule_weights > 0],
+      weights = rule_weights[rule_weights > 0],
+      class = class,
+      levels = lvls,
+      default = levels(rightHand)[defaultClass],
+      description = description,
+      method = "weighted"
+    )
+
+
+  } else {
+    stop("Method must be one of: 'CBA', 'weighted'.")
+  }
+
+  class(classifier) <- "CBA"
+
+  return(classifier)
+
+}
