3d170cfe0c91922013d87df090a41f1166ca7a73
Ian Johnson <ianjjohnson@icloud.com>
2017-03-03 12:36:40 -0600

    update tests to reflect new default lhs.support functionality

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -7,7 +7,7 @@ data(iris)
 irisDisc <- as.data.frame(lapply(iris[1:4], function(x)
   discretize(x, categories=9)))
 irisDisc$Species <- iris$Species
-classifier <- CBA(Species ~ ., irisDisc, supp = 0.05, conf=0.9)
+classifier <- CBA(Species ~ ., irisDisc, supp = 0.05, conf=0.9, lhs.support=FALSE)
 results <- predict(classifier, irisDisc)
 
 expect_equal(length(classifier$rules), 11L)
