19ccb42621f6e62482a56d8c1efa3eb6bca0a54f
Ian Johnson <ianjjohnson@icloud.com>
2017-05-08 10:49:11 -0500

    add wCBA.R

---
+++ R/wCBA.R
@@ -0,0 +1,10 @@
+wCBA <- function(formula, data, support = 0.2, confidence = 0.8,
+  verbose=FALSE, parameter = NULL, control = NULL, sort.parameter = NULL, lhs.support=FALSE, class.weights=NULL,
+	disc.method = "cluster", disc.categories = 10){
+
+    return(CBA.internal(formula, data, method="weighted", support = support,
+      confidence = confidence, gamma = gamma, cost = cost, verbose = verbose,
+      parameter = parameter, control = control, sort.parameter = sort.parameter,
+      lhs.support = lhs.support, disc.method=disc.method, disc.categories=disc.categories))
+
+}
