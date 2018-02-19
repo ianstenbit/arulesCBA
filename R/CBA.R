1ab8bc11ef3454b2f88ab435e40b063cd74112d9
Ian Johnson <ianjjohnson@mac.com>
2018-02-19 11:36:25 -0600

    refactor

--- R/CBA.R
+++ R/CBA.R
@@ -1,9 +1,9 @@
-CBA <- function(formula, data, support = 0.2, confidence = 0.8, verbose = FALSE,
-  parameter = NULL, control = NULL, sort.parameter = NULL, lhs.support = FALSE,
-	disc.method = "cluster", disc.categories = 10){
+CBA <- function(rules, data, class, disc.data, method="CBA", support = 0.2, confidence = 0.8, gamma = 0.05, cost = 10.0,
+  verbose=FALSE, parameter = NULL, control = NULL, sort.parameter=NULL, lhs.support=TRUE, class.weights=NULL,
+  disc.categories = 10, disc.method="cluster", sort.rules=FALSE){
 
-    return(CBA.internal(formula, data, method="CBA", support = support, confidence = confidence,
-      verbose=FALSE, parameter = NULL, control = NULL, sort.parameter = sort.parameter, lhs.support=lhs.support,
-			disc.method = "cluster", disc.categories = 10))
+    return(CBA.internal(rules, data, class, disc.data, method="CBA", support = 0.2, confidence = 0.8, gamma = 0.05, cost = 10.0,
+      verbose=FALSE, parameter = NULL, control = NULL, sort.parameter=NULL, lhs.support=TRUE, class.weights=NULL,
+      disc.categories = 10, disc.method="cluster", sort.rules=FALSE))
 
 }
