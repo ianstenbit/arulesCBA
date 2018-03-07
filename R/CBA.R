cdee61344e4d4af89ccfc74cf048026c3dee5a89
Ian Johnson <ianjjohnson@mac.com>
2018-03-07 07:58:05 -0600

    working on discretization fixes

--- R/CBA.R
+++ R/CBA.R
@@ -1,9 +1,9 @@
 CBA <- function(formula, data, support = 0.2, confidence = 0.8, verbose = FALSE,
   parameter = NULL, control = NULL, sort.parameter = NULL, lhs.support = FALSE,
-	disc.method = "cluster", disc.categories = 10){
+	disc.method = "mldp", disc.categories = 10){
 
     return(CBA.internal(formula, data, method="CBA", support = support, confidence = confidence,
       verbose=FALSE, parameter = NULL, control = NULL, sort.parameter = sort.parameter, lhs.support=lhs.support,
-			disc.method = "cluster", disc.categories = 10))
+			disc.method = disc.method, disc.categories = 10))
 
 }
--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -4,8 +4,6 @@ library("arulesCBA")
 context("arulesCBA Interface")
 
 data(iris)
-irisDisc <- as.data.frame(lapply(iris[1:4], function(x)
-  discretize(x, categories=9)))
 irisDisc$Species <- iris$Species
 classifier <- CBA(Species ~ ., irisDisc, supp = 0.05, conf=0.9, lhs.support=FALSE)
 results <- predict(classifier, irisDisc)
