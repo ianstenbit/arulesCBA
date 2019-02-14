7ba02467f0a03a48873547edfe0dbe6a7e06c901
Tyler Giallanza <tylergiallanza@gmail.com>
2019-02-14 11:34:29 -0600

    Add tests for RCAR; make CBA object

--- R/RCAR.R
+++ R/RCAR.R
@@ -1,25 +1,19 @@
 RCAR <- function(formula, data, support = 0.3, confidence = 0.7, verbose = FALSE,
                  maxlen = 6, lambda = 0.001, balanceSupport = FALSE) {
   trans <- as(data,'transactions')
-  rules <- mineCARs(formula, trans, balanceSupport, parameter=list(maxlen=maxlen), 
+  rules <- mineCARs(formula, trans, balanceSupport, parameter=list(supp=support,conf=confidence,maxlen=maxlen), 
                     control=list(verbose=verbose))
   X <- is.superset(trans,as(rules@lhs,'itemMatrix'))
   y_class <- .parseformula(formula, data)$class_names
   model <- glmnet(X,data[[y_class]],family='multinomial',alpha=1,lambda=lambda)
-  classifier <- list(rules=rules,model=model,name='RCAR classifier')
-  class(classifier) <- 'RCAR'
+  num_nonzero_rules <- sum(unlist(lapply(model$beta, function(x) sum(x>0))))
+  classifier <- list(rules=rules,model=model,method='RCAR classifier',class=model$classnames,default=model$classnames[[1]],
+                     description='RCAR algorithm by Azmi et al. 2019')
+  class(classifier) <- c('RCAR','CBA')
   
   return(classifier)
 }
 
-print.RCAR <- function(x, ...) {
-  num_rules <- lapply(x$model$beta, function(x) sum(x>0))
-  writeLines(c(
-    "RCAR Classifier Object",
-    paste("Number of rules:", sum(unlist(num_rules))),
-    ""
-  ))
-}
 
 predict.RCAR <- function(object, newdata, ...){
   rules.space<-as(object[[1]]@lhs,"itemMatrix")
